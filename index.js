0963471467379a73b62af07ac624413bcd8bf923
Tauhida Parveen <tparveen@users.noreply.github.com>
2016-10-25 00:49:21 -0400

    Add files via upload

---
+++ index.js
@@ -0,0 +1,30 @@
+var express = require('express');
+var bodyParser = require('body-parser');
+var mongoose = require('mongoose');
+
+var app = express();
+
+var jsonParser = bodyParser.json();
+
+// Add your API endpoints here
+
+var runServer = function(callback) {
+    var databaseUri = process.env.DATABASE_URI || global.databaseUri || 'mongodb://localhost/sup';
+    mongoose.connect(databaseUri).then(function() {
+        var port = process.env.PORT || 8080;
+        var server = app.listen(port, function() {
+            console.log('Listening on localhost:' + port);
+            if (callback) {
+                callback(server);
+            }
+        });
+    });
+};
+
+if (require.main === module) {
+    runServer();
+};
+
+exports.app = app;
+exports.runServer = runServer;
+
